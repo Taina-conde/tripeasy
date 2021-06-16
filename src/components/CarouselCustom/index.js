fd3463bc63ab0ffc79df325dbb04dcfba70d787f
Taina Conde <tainabatistaconde@gmail.com>
2021-06-16 13:51:25 -0400

    feat: Create CarouselCustom component

---
+++ src/components/CarouselCustom/index.js
@@ -0,0 +1,46 @@
+import Carousel from "react-bootstrap/Carousel";
+
+const CarouselCustom = () => {
+  return (
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src=""
+          alt=""
+        />
+        <Carousel.Caption>
+          <h3></h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src=""
+          alt=""
+        />
+
+        <Carousel.Caption>
+          <h3></h3>
+          <p></p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src=""
+          alt=""
+        />
+
+        <Carousel.Caption>
+          <h3></h3>
+          <p>
+            
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+};
+export default CarouselCustom;
