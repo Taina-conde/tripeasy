621616b2200975e2bfec412a1d73f7b64cefa7ec
Taina Conde <tainabatistaconde@gmail.com>
2021-06-16 17:45:51 -0400

    feat: SignUpForm - username validation

--- src/components/InitialForm/SignUpForm.js
+++ src/components/InitialForm/SignUpForm.js
@@ -1,4 +1,74 @@
-const SignUpForm = ()=> {
-    return ()
-}
+import { useFormik } from "formik";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const SignUpForm = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      email: "",
+      password: "",
+    },
+    validate: (values) => {
+      const errors = {};
+      if (!values.username) {
+        errors.username = "Required";
+      }
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email address";
+      }
+      if (!values.password) {
+        errors.password = "Required";
+      } else if (!/^[A-Za-z]\w{7,14}$/i.test(values.password)) {
+        errors.password = "Invalid password";
+      }
+      return errors;
+    },
+    onSubmit: (values) => {},
+  });
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <Row className="mb-3">
+        <Form.Group as={Col} controlId="formUsername">
+          <Form.Label>Username ICON</Form.Label>
+          <Form.Control
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={formik.values.username}
+            onChange={formik.onChange}
+            isInvalid={!!formik.errors.username}
+          />
+          <Form.Control.Feedback type="invalid">
+            {errors.username}
+          </Form.Control.Feedback>
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+};
 export default SignUpForm;
