2aab2efe3e12ec421c04e30c8631759e05a236e2
Taina Conde <tainabatistaconde@gmail.com>
2021-06-16 16:51:04 -0400

    feat: SignUpForm - create component with minimal code

--- src/components/InitialForm/index.js
+++ src/components/InitialForm/index.js
@@ -3,7 +3,8 @@ import React, {useState} from "react";
 const InitialForm = () => {
   const [signUp, setSignUp] = useState(true)
   return <div>
-      <h1 className = "display-6">{signUp ? "Create your account" : "Sign in "}</h1>
+      <h1>{signUp ? "Create your account" : "Sign in "}</h1>
+      
   </div>;
 };
 export default InitialForm;
