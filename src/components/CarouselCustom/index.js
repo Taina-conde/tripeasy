d49f5e4d854d90c6c97d69ca4a11e9adae976685
Taina Conde <tainabatistaconde@gmail.com>
2021-06-17 18:38:21 -0400

    refactor: CarouselCustom - remove captions, indicators and change icons
    The reason of these changes is to make the layout and
    quantity of information according to model 1 of the
    given design.

--- src/components/CarouselCustom/index.js
+++ src/components/CarouselCustom/index.js
@@ -5,46 +5,27 @@ import restaurant from "../../assets/images/jay-wennington-N_Y88TWmGwA-unsplash.
 import schedule from "../../assets/images/sonja-langford-eIkbSc3SDtI-unsplash.jpg";
 import budget from "../../assets/images/rupixen-com-pbgYcq3_Zx0-unsplash.jpg";
 import "./CarouselCustom.scss";
+import { IoIosArrowDropright } from "react-icons/io";
+import { IoIosArrowDropleft } from "react-icons/io";
 
 const CarouselCustom = () => {
   return (
-    <Carousel className = "h-100">
+    <Carousel className = "h-100" indicators ={false} nextIcon = {<span><IoIosArrowDropright size= {40}/></span>} prevIcon = {<span><IoIosArrowDropleft size= {40}/></span>}>
       <Carousel.Item>
         <img className="d-block " src={trip} alt="trip" />
-        <Carousel.Caption>
-          <h3>Manage your trips</h3>
-          <p>TripEasy helps you manage your trips so you can have a stress free well-deserved vacation</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block " src={hotel} alt="hotel" />
-        <Carousel.Caption>
-          <h3>Accomodation</h3>
-          <p>Find your ideal accomodation in your destination and manage your bookings.</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block " src={restaurant} alt="restaurant" />
-
-        <Carousel.Caption>
-          <h3>Taste the best food in town</h3>
-          <p>TripEasy helps you find and make reservations in the most recommended restaurants in your destination</p>
-        </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block " src={schedule} alt="schedule" />
-        <Carousel.Caption>
-          <h3>Daily Schedule</h3>
-          <p>We automatically save your flight, accomodation and restaurant reservations details in your daily schedule so you won't miss anything</p>
-        </Carousel.Caption>
+      
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block " src={budget} alt="budget" />
-
-        <Carousel.Caption>
-          <h3>Stay on budget</h3>
-          <p> You can scan your receipts or enter other expenses to track how much you are spending</p>
-        </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
   );
