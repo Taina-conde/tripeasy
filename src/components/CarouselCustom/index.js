2fecddba3f5d6e964be36f9116298378dfe9be9b
Taina Conde <tainabatistaconde@gmail.com>
2021-06-17 19:12:42 -0400

    style: CarouselCustom - add z-index to controls
    The next control was in front of the prev control, which
    made the prev control unclickable. Add z-indexes to fix
    that problem.

--- src/components/CarouselCustom/index.js
+++ src/components/CarouselCustom/index.js
@@ -1,6 +1,6 @@
 import Carousel from "react-bootstrap/Carousel";
 import trip from "../../assets/images/annie-spratt-qyAka7W5uMY-unsplash.jpg";
-import hotel from "../../assets/images/paul-postema-mr0Dp231IEw-unsplash.jpg";
+import hotel from "../../assets/images/mike-swigunski-k9Zeq6EH_bk-unsplash.jpg";
 import restaurant from "../../assets/images/jay-wennington-N_Y88TWmGwA-unsplash.jpg";
 import schedule from "../../assets/images/sonja-langford-eIkbSc3SDtI-unsplash.jpg";
 import budget from "../../assets/images/rupixen-com-pbgYcq3_Zx0-unsplash.jpg";
@@ -10,7 +10,20 @@ import { IoIosArrowDropleft } from "react-icons/io";
 
 const CarouselCustom = () => {
   return (
-    <Carousel className = "h-100" indicators ={false} nextIcon = {<span><IoIosArrowDropright size= {40}/></span>} prevIcon = {<span><IoIosArrowDropleft size= {40}/></span>}>
+    <Carousel
+      className="h-100"
+      indicators={false}
+      nextIcon={
+        <span>
+          <IoIosArrowDropright size={40} />
+        </span>
+      }
+      prevIcon={
+        <span>
+          <IoIosArrowDropleft size={40} />
+        </span>
+      }
+    >
       <Carousel.Item>
         <img className="d-block " src={trip} alt="trip" />
       </Carousel.Item>
@@ -22,7 +35,6 @@ const CarouselCustom = () => {
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block " src={schedule} alt="schedule" />
-      
       </Carousel.Item>
       <Carousel.Item>
         <img className="d-block " src={budget} alt="budget" />
