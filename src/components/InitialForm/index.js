a67cc937579d43feab3b62d88b84aa0cd7df7bbe
Taina Conde <tainabatistaconde@gmail.com>
2021-06-21 17:09:40 -0400

    style: InitialForm primary btn - change color in focus

--- src/components/InitialForm/index.js
+++ src/components/InitialForm/index.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import SignUpForm from "./SignUpForm";
 import SignInForm from "./LoginForm";
 import Container from "react-bootstrap/Container";
-import './styles.scss';
-
+import "./styles.scss";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const InitialForm = () => {
   const [signUp, setSignUp] = useState(true);
@@ -12,17 +13,29 @@ const InitialForm = () => {
     setSignUp(!signUp);
   };
   return (
-    <Container>
-      <h1>
-        <span className = "logo-trip">Trip</span>
-        <span className = "logo-easy">Easy</span>
-      </h1>
-      <h2>{signUp ? "Create your account" : "Sign in "}</h2>
-      {signUp ? (
-        <SignUpForm onToggleSignUp={toggleSignUpHandler} />
-      ) : (
-        <SignInForm onToggleSignUp={toggleSignUpHandler} />
-      )}
+    <Container className="mt-5">
+      <Row>
+        <Col>
+          <h1>
+            <span className="logo-trip">Trip</span>
+            <span className="logo-easy">Easy</span>
+          </h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h2>{signUp ? "Create your account" : "Sign in "}</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          {signUp ? (
+            <SignUpForm onToggleSignUp={toggleSignUpHandler} />
+          ) : (
+            <SignInForm onToggleSignUp={toggleSignUpHandler} />
+          )}
+        </Col>
+      </Row>
     </Container>
   );
 };
